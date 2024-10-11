fb28965ff383a308dab6098bdb2f816dc0850b9b
khushi varshney <anuragkhushi703@gmail.com>
2024-10-11 21:46:30 +0530

    ADD some new features

--- backEnd/index.js
+++ backEnd/index.js
@@ -5,7 +5,13 @@ import mongoose from "mongoose";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST", "GET"],
+    credentials: true
+  }
+));
 
 mongoose
   .connect("mongodb://localhost:27017/myLoginRegisterDB", {
