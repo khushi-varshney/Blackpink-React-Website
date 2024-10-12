484b277b3d2e0c60d222df19c457167d4128c0d2
khushi varshney <anuragkhushi703@gmail.com>
2024-10-12 21:41:30 +0530

    add features

--- frontEnd/src/Components/About.jsx
+++ frontEnd/src/Components/About.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const About = () => {
   return (
-    <div className='flex flex-col items-center justify-center gap-3 px-10 py-3 border-pink-500 border-2'>
+    <div className='flex flex-col items-center justify-center gap-3 px-10 py-7 border-pink-500 border-2'>
       <h1 className='text-3xl font-extrabold'>BLACKPINK FAN HUB</h1>
       <p className='md:text-[2vw] text-[4vw] p-1 text-pink-500 font-bold'>A Comprehensive Online Platform For Blackpink Enthusiasts</p>
       <p className='text-lg font-semibold'>Hello, I Khushi Varshney, is very pleased to introduce the "BLACKPINK FAN HUB", a meticulously designed online platform dedicated to providing an immersive experience for fans of the renowned K-POP Group, BLACKPINK. This Website is for all of those BLINKS like me who have loved BLACKPINK infinitely and will continue to adore, respect and protect them. As a Fan-Driven Project, This website is created for the BLINKS so that they can unite, share and indulge in their Love for Rose, Jennie, Lisa and Jisoo.</p>
