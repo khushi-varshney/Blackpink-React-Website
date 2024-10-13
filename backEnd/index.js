385bd4ea308aacbce0398de8651157efef4b7401
khushi varshney <anuragkhushi703@gmail.com>
2024-10-13 22:36:39 +0530

    add

--- backEnd/index.js
+++ backEnd/index.js
@@ -6,10 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', req.header('origin') );
-  next();
-});
+
 
 app.use(cors(
   {
