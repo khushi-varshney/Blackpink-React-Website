20667a58273a92bb895260baa66ea6ffa8661c8d
khushi varshney <anuragkhushi703@gmail.com>
2024-10-24 22:56:50 +0530

    add

--- backEnd/index.js
+++ backEnd/index.js
@@ -7,18 +7,17 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 // app.use(cors());
-app.use(cors({
-  origin: ['https://blackpink-mu.vercel.app/', 'http://localhost:5173/'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}))
+// app.use(cors({
+//   origin: ['https://blackpink-mu.vercel.app/', 'http://localhost:5173/'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// }))
 
-
-app.use((req,res,next)=>{
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Alow-Methods', 'GET,POST,PUT,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 })
 
