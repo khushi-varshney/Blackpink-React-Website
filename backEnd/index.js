a5b9d8776a859cd2b607ee0bff4cf20d794a6a7c
khushi varshney <anuragkhushi703@gmail.com>
2024-10-25 12:10:20 +0530

    add

--- backEnd/index.js
+++ backEnd/index.js
@@ -21,9 +21,7 @@ app.use((req, res, next) => {
   next();
 })
 
-const DB = "mongodb+srv://user:koHKbEuxFTqC6OSk@cluster0.4lsix.mongodb.net/mernstack?retryWrites=true&w=majority&appName=Cluster0"
-
-mongoose.connect(DB, {
+mongoose.connect(process.env.MONGO_DB, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 })
